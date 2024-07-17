c669a1030183854e7f8dfddf90f3c64015cea0f7
Aman7073 <amanmittaljaipur@gmail.com>
2024-07-18 00:40:14 +0530

    Changes in the DAO and the Business Layer.

--- JEDI-2.0-FlipFit-Group-F-Java-POS/src/com/flipkart/dao/BookGymDAOImpl.java
+++ JEDI-2.0-FlipFit-Group-F-Java-POS/src/com/flipkart/dao/BookGymDAOImpl.java
@@ -1,5 +1,28 @@
 package com.flipkart.dao;
 
-public class BookGymDAOImpl {
+import com.flipkart.bean.Booking;
 
+import java.util.List;
+
+public class BookGymDAOImpl implements BookGymDAOInterface{
+
+    @Override
+    public void createBooking(Booking booking) {
+
+    }
+
+    @Override
+    public void bookSlots(Booking booking) {
+
+    }
+
+    @Override
+    public List<Booking> viewBookings(int userId) {
+        return List.of();
+    }
+
+    @Override
+    public void cancelBookings(int bookingId) {
+
+    }
 }
