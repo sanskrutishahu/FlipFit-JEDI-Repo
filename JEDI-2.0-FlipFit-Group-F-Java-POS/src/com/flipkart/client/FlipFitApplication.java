f94909125c151bdb903020ffbfc0365d076d2c38
tushmahe <lci2020023@iiitl.ac.in>
2024-07-17 14:54:00 +0530

    Revert "resolved errors"
    This reverts commit fda2309efc35dc9b4f2922afb1dca60799b767c5.
    
    t

--- JEDI-2.0-FlipFit-Group-F-Java-POS/src/com/flipkart/client/FlipFitApplication.java
+++ JEDI-2.0-FlipFit-Group-F-Java-POS/src/com/flipkart/client/FlipFitApplication.java
@@ -82,7 +82,6 @@ public class FlipFitApplication {
                 registerUser();
                 break;
             case 3:
-            	System.out.println("You are in change password section");
                 break;
             case 4:
                 System.out.println("Thank you for using FlipFit App");
